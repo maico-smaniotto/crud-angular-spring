863bcbcf46e65e57c060bf54ba835b8a9534a1cb
maico-smaniotto <maicosmaniotto@gmail.com>
2025-03-21 23:29:31 -0300

    Início dos testes unitários da controller

---
+++ crud-spring/src/test/java/com/maicosmaniotto/crud_spring/controller/CourseControllerTest.java
@@ -0,0 +1,81 @@
+package com.maicosmaniotto.crud_spring.controller;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.maicosmaniotto.crud_spring.config.ValidationAdvice;
+import com.maicosmaniotto.crud_spring.data.CourseTestData;
+import com.maicosmaniotto.crud_spring.dto.CourseDTO;
+import com.maicosmaniotto.crud_spring.dto.PageDTO;
+import com.maicosmaniotto.crud_spring.dto.mapper.CourseMapper;
+import com.maicosmaniotto.crud_spring.model.Course;
+import com.maicosmaniotto.crud_spring.service.CourseService;
+
+@ActiveProfiles("test")
+@SpringJUnitConfig(classes = {CourseController.class})
+class CourseControllerTest {
+
+    private static final String API = "/api/courses";
+    private static final String API_ID = "/api/courses/{id}";
+
+    @InjectMocks
+    private CourseController courseController;
+
+    @Mock
+    private CourseService courseService;
+
+    @Spy
+    private CourseMapper mapper = new CourseMapper();
+
+    @BeforeEach
+    void setUp() {
+        ProxyFactory factory = new ProxyFactory(courseController);
+        factory.addAdvice(new ValidationAdvice());
+        courseController = (CourseController) factory.getProxy();
+    }
+
+    @Test
+    @DisplayName("Should return a list of courses in JSON format")
+    private void testList() throws Exception {
+        Course c = CourseTestData.createValidCourseWithOneLesson();
+        CourseDTO dto = mapper.toDTO(c);
+        List<CourseDTO> courses = List.of(dto);
+        PageDTO<CourseDTO> page = new PageDTO<>(courses, 1, 1, 1, 0);
+
+        when(courseService.list(anyInt(), anyInt())).thenReturn(page);
+        MockMvcBuilders.standaloneSetup(courseController)
+            .build()
+            .perform(MockMvcRequestBuilders.get(API))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.content", hasSize(courses.size())))
+            .andExpect(jsonPath("totalElements", is(1)))
+            .andExpect(jsonPath("$.content[0].id", is(dto.id()), Long.class))
+            .andExpect(jsonPath("$.content[0].name", is(dto.name())))
+            .andExpect(jsonPath("$.content[0].category", is(dto.category())))
+            .andExpect(jsonPath("$.content[0].lessons", hasSize(dto.lessons().size())))
+            .andExpect(jsonPath("$.content[0].lessons[0].id", is(dto.lessons().get(0).id()), Long.class));
+
+    }
+}
