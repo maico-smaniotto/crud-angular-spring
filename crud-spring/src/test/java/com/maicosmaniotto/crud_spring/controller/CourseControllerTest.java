f181cdb61c7e74efcda1b69b0a2d339f0b59c494
maico-smaniotto <maicosmaniotto@gmail.com>
2025-03-24 23:58:57 -0300

    Testes unitários para a classe CourseController (localizar criar e atualizar curso)

--- crud-spring/src/test/java/com/maicosmaniotto/crud_spring/controller/CourseControllerTest.java
+++ crud-spring/src/test/java/com/maicosmaniotto/crud_spring/controller/CourseControllerTest.java
@@ -1,16 +1,22 @@
 package com.maicosmaniotto.crud_spring.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,13 +31,17 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.maicosmaniotto.crud_spring.config.ValidationAdvice;
 import com.maicosmaniotto.crud_spring.data.CourseTestData;
 import com.maicosmaniotto.crud_spring.dto.CourseDTO;
 import com.maicosmaniotto.crud_spring.dto.PageDTO;
 import com.maicosmaniotto.crud_spring.dto.mapper.CourseMapper;
+import com.maicosmaniotto.crud_spring.exception.RecordNotFoundException;
 import com.maicosmaniotto.crud_spring.model.Course;
 import com.maicosmaniotto.crud_spring.service.CourseService;
+
+import jakarta.servlet.ServletException;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -103,4 +113,174 @@ class CourseControllerTest {
             .andExpect(jsonPath("lessons", hasSize(courseDTO.lessons().size())))
             .andExpect(jsonPath("lessons[0]._id", is(courseDTO.lessons().get(0).id()), Long.class));
     }
+
+    @Test
+    @DisplayName("Should throw ConstraintViolationException when id is negative")
+    void testFindByIdNegative() {              
+        var requestBuilder = MockMvcRequestBuilders.get(API_ID, -1L);
+        assertThrows(ServletException.class, () -> {
+            MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(status().isBadRequest());
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw RecordNotFoundException when course is not found")
+    void testFindByIdNotFound() {
+        when(courseService.findById(anyLong())).thenThrow(RecordNotFoundException.class);
+
+        var requestBuilder = MockMvcRequestBuilders.get(API_ID, 1L);
+        assertThrows(ServletException.class, () -> {
+            MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(status().isNotFound());
+        });
+    }
+
+    @Test
+    @DisplayName("Should create a new course")
+    void testCreate() throws Exception {
+        Course course = CourseTestData.createValidCourseWithOneLesson();
+        CourseDTO courseDTO = courseMapper.toDTO(course);
+
+        when(courseService.create(courseDTO)).thenReturn(courseDTO);
+
+        var contentJson = new ObjectMapper().writeValueAsString(courseDTO);
+        var requestBuilder = MockMvcRequestBuilders.post(API)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(contentJson);
+        MockMvcBuilders.standaloneSetup(courseController)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(status().isCreated())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("_id", is(courseDTO.id()), Long.class))
+            .andExpect(jsonPath("name", is(courseDTO.name())))
+            .andExpect(jsonPath("category", is(courseDTO.category())))
+            .andExpect(jsonPath("lessons", hasSize(courseDTO.lessons().size())))
+            .andExpect(jsonPath("lessons[0]._id", is(courseDTO.lessons().get(0).id()), Long.class));
+    }
+
+    @Test
+    @DisplayName("Should return bad request when trying to create a course with invalid data")
+    void testCreateInvalid() throws Exception {
+        List<CourseDTO> courses = new ArrayList<>();
+        Course course;
+
+        course = CourseTestData.createValidCourseWithOneLesson();
+        course.setName("");
+        courses.add(courseMapper.toDTO(course));
+
+        course = CourseTestData.createValidCourseWithOneLesson();
+        course.getLessons().clear();
+        courses.add(courseMapper.toDTO(course));
+        
+        for (CourseDTO courseDTO : courses) {
+            var contentJson = new ObjectMapper().writeValueAsString(courseDTO);
+            var requestBuilder = MockMvcRequestBuilders.post(API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(contentJson);
+            MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Test
+    @DisplayName("Should update a course")
+    void testUpdate() throws Exception {
+        Course course = CourseTestData.createValidCourseWithOneLesson();
+        course.setId(1L);
+        CourseDTO courseDTO = courseMapper.toDTO(course);
+
+        when(courseService.update(anyLong(), any())).thenReturn(courseDTO);
+
+        var contentJson = new ObjectMapper().writeValueAsString(courseDTO);
+        var requestBuilder = MockMvcRequestBuilders.put(API_ID, course.getId())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(contentJson);
+        MockMvcBuilders.standaloneSetup(courseController)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("_id", is(courseDTO.id()), Long.class))
+            .andExpect(jsonPath("name", is(courseDTO.name())))
+            .andExpect(jsonPath("category", is(courseDTO.category())))
+            .andExpect(jsonPath("lessons", hasSize(courseDTO.lessons().size())))
+            .andExpect(jsonPath("lessons[0]._id", is(courseDTO.lessons().get(0).id()), Long.class));
+    }
+
+    @Test
+    @DisplayName("Should throw RecordNotFoundException when trying to update a course that does not exist")
+    void testUpdateNotFound() throws Exception {
+        Course course = CourseTestData.createValidCourseWithOneLesson();
+        course.setId(1L);
+        CourseDTO courseDTO = courseMapper.toDTO(course);
+
+        when(courseService.update(anyLong(), any())).thenThrow(RecordNotFoundException.class);
+
+        var contentJson = new ObjectMapper().writeValueAsString(courseDTO);
+        var requestBuilder = MockMvcRequestBuilders.put(API_ID, course.getId())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(contentJson);
+        assertThrows(ServletException.class, () -> {
+            MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(status().isNotFound());
+        });
+    }
+
+    @Test
+    @DisplayName("Should return bad request when trying to update a course with invalid data")
+    void testUpdateInvalid() throws Exception {
+        Map<Long, CourseDTO> courses = new HashMap<>();
+        Course course;
+
+        // Valid id and invalid data
+        course = CourseTestData.createValidCourseWithOneLesson();
+        course.setName("");
+        courses.put(1L, courseMapper.toDTO(course));
+        course = CourseTestData.createValidCourseWithOneLesson();
+        course.getLessons().clear();
+        courses.put(2L, courseMapper.toDTO(course));
+
+        for (Map.Entry<Long, CourseDTO> entry : courses.entrySet()) {
+            var contentJson = new ObjectMapper().writeValueAsString(entry.getValue());
+            var requestBuilder = MockMvcRequestBuilders.put(API_ID, entry.getKey())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(contentJson);
+            MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(status().isBadRequest());
+        }
+
+        courses.clear();
+
+        // Invalid id and valid data
+        course = CourseTestData.createValidCourseWithOneLesson();
+        courses.put(0L, courseMapper.toDTO(course));
+        course = CourseTestData.createValidCourseWithOneLesson();
+        courses.put(-1L, courseMapper.toDTO(course));
+
+        for (Map.Entry<Long, CourseDTO> entry : courses.entrySet()) {            
+            var contentJson = new ObjectMapper().writeValueAsString(entry.getValue());
+            var requestBuilder = MockMvcRequestBuilders.put(API_ID, entry.getKey())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(contentJson);
+            assertThrows(ServletException.class, () -> {
+                MockMvcBuilders.standaloneSetup(courseController)
+                    .build()
+                    .perform(requestBuilder)
+                    .andExpect(status().isBadRequest());
+            });
+        }
+    }
+
 }
